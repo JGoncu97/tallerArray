a7e06f4423a875c2b0c58faef96f21090bc7a3c2
Jose <Sena@ARMAPRCCIFOD560.FormacionSena.RED>
2024-03-05 11:04:02 -0500

    Array de diferentes elementos

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ let arrayVacio=[]
 
 //2. Crear un array con elementos:
 let arrayElements=[1,2,3,4,5,6]
+
+//3. Crear un array con elementos de diferentes tipos de datos:
+let arrayElementsDiferent=[1,"hola",true,cubo,false]
+
