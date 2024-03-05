465958174a1f8333a8e7b04327a552cb2415f9d5
Jose <Sena@ARMAPRCCIFOD560.FormacionSena.RED>
2024-03-05 11:17:22 -0500

    Array constructor

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ let arrayElements=[1,2,3,4,5,6]
 //3. Crear un array con elementos de diferentes tipos de datos:
 let arrayElementsDiferent=[1,"hola",true,cubo,false]
 
+//4. Crear un array utilizando el constructor Array():
+const language = new Array("HTML", "CSS", "Javascript"); 
+
